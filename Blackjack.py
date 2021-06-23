a4364fc62184508687e337eec5c537618978b6af
Denis Murphy <djmurphy2001@gmail.com>
2021-06-23 11:11:02 -0230

    First commit

---
+++ Blackjack.py
@@ -0,0 +1,13 @@
+import random
+import csv
+
+def display_title():
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+    print()
+
+def main():
+    display_title()
+
+if __name__ == "__main__":
+    main()
